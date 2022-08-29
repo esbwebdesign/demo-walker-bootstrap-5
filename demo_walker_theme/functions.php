56e334e12c80020a13f605eb257c32c24844dd55
esbwebdesign <31042473+esbwebdesign@users.noreply.github.com>
2022-08-29 11:10:43 -0500

    Add Instructions- start_el, start_lvl
    feat: Add additional instructions for start_el, start_lvl

--- demo_walker_theme/functions.php
+++ demo_walker_theme/functions.php
@@ -10,10 +10,38 @@ class bootstrap_walker extends Walker_Nav_Menu {
 		function start_el(&$output, $item, $depth = 0, $args = null, $id = 0) {
 			$li_class[] = 'nav-item';
 			$a_class[] = 'nav-link';
+			$aria = '';
+			$a_dropdown = '';
 			
-			$link_tag = '<a class="' . implode(' ', $a_class) . '" href="' . $item->url . '">';
+			if($item->current || $item->current_item_ancestor) {
+				$a_class[] = 'active';
+				if($item->current) {
+					$aria = ' aria-current="page"';
+				}
+			}
+			
+			if($item->menu_order != 1 && !$item->menu_item_parent) {
+				$li_class[] = 'ms-lg-5';
+			}
+			
+			if($args->walker->has_children && $depth == 0) {
+				$li_class[] = 'dropdown';
+				$a_class[] = 'dropdown-toggle';
+				$item->url = '#';
+				$a_dropdown = ' id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"';
+			}
+			
+			$link_tag = '<a class="' . implode(' ', $a_class) . $a_dropdown . $aria . '" href="' . $item->url . '">';
 			
 			$output .= '<li class="' . implode(' ', $li_class) . '">' . $link_tag . $item->title . '</a>';
+		}
+		
+		function start_lvl(&$output, $depth = 0, $args = null) {
+			if(!$depth) {
+				$output .= '<ul class="dropdown-menu" aria-labelledby="navbarDropdown">';
+			} else {
+				$output .= '<ul>';
+			}
 		}
 }
 
